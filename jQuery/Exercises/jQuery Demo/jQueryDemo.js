a857cf379f0da7d148c809054955fc7507b7223e
bgangware <68155775+bgangware@users.noreply.github.com>
2020-08-26 15:19:03 -0400

    Added jQuery selector command expamples

--- jQuery/Exercises/jQuery Demo/jQueryDemo.js
+++ jQuery/Exercises/jQuery Demo/jQueryDemo.js
@@ -1,8 +1,12 @@
 window.setTimeout(function() {
 alert("Hello");
-
-
-
+$("h1").css("color","green");
+var selstyles = {
+    color: "blue",
+    backgroundColor: "yellow",
+    fontWeight: "bold"
+}
+$("#adorable").css(selstyles);
 
 
 }, 500);
