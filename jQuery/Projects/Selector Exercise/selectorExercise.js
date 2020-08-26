802b9990f6755847e8bfe1da4346e19a11090fca
bgangware <68155775+bgangware@users.noreply.github.com>
2020-08-26 15:58:31 -0400

    Added jQuery selector exercise

---
+++ jQuery/Projects/Selector Exercise/selectorExercise.js
@@ -0,0 +1,11 @@
+window.setTimeout(function() {
+alert("Hello");
+$("div").css("background","purple");
+$(".highlight").css("width","200px");
+$("#third").css("border","3px solid orange");
+
+
+
+
+
+}, 500);
