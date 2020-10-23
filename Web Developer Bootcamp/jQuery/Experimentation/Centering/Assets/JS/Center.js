3dbe13df2436322ff81d1b4918f0ba26bedb22ad
bgangware <68155775+bgangware@users.noreply.github.com>
2020-10-22 22:19:58 -0400

    Update to to do list

---
+++ Web Developer Bootcamp/jQuery/Experimentation/Centering/Assets/JS/Center.js
@@ -0,0 +1,8 @@
+window.setTimeout(function() {
+alert("Hello Brand");
+$("h1").text("jQuery is Working"); 
+
+
+
+
+}, 500);
--- Web Developer Bootcamp/jQuery/Projects/To Do List/Assets/JS/toDoList.js
+++ Web Developer Bootcamp/jQuery/Projects/To Do List/Assets/JS/toDoList.js
@@ -1,8 +1,69 @@
 window.setTimeout(function() {
-alert("Hello Brand");
-$("h1").text("jQuery is Working"); 
+/* alert("Hello Brand");
+$("h1").text("jQuery is Working");  */
+
+// $("li").css("background","lime");
+
+setListItemBackground();
+
+function setListItemBackground()
+{
+    console.log("Working");
+    var listItem = document.querySelectorAll("li");
+
+    var i;
+    for (i = 0; i < listItem.length; i++) {
+        var i2 = i+2;
+        if (i2%2 == 0) { 
+        console.log(i, i2);
+        listItem[i].style.background = "white"; 
+        }
+        else {
+        listItem[i].style.background = "#f7f7f7"; 
+        }
+    }
+} 
+
+function addToDo()
+{
+    console.log("Working");
+    
+    var appText =  $("input:text").val();
+    var appTextIns = "<li>" + appText + "</li>";
+    $("input:text").val("")
+    $( "ul" ).append( appTextIns );
+}
+
+$("#plus").click(function() {
+    var x = document.querySelector("input");
+        if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            ;}  
+});
+
+$('input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13' && $("input").val().length != 0) {
+        addToDo();
+        setListItemBackground()
+        }
+        
+});
 
 
+$("ul").on("click","li",function() {
+    $(this).toggleClass("completedTodo");
+    console.log("li clicked");
+    
+});
 
-
+$("li").hover(function() {
+    // $(this).toggleClass("completedTodo");
+    // $(this).css("display", "inline");
+    // let span=$(this).getElements("span");
+    console.log("li hover");
+    
+});
 }, 500);
